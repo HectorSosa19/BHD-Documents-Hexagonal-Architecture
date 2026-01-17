3d6b1e9f9647d0465902379017ee916a9af15007
Hector Jose Sosa Castro <hectososa19@Hectors-MacBook-Pro.local>
2026-01-17 11:43:15 -0400

    deleting s of docker-compose file

--- BHD.Documents.API/obj/Debug/net8.0/BHD.Documents.API.AssemblyInfo.cs
+++ BHD.Documents.API/obj/Debug/net8.0/BHD.Documents.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BHD.Documents.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdce03267ff43361320f596e68d12e25fbfa5bc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BHD.Documents.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BHD.Documents.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
